89a8d8c8c17e5b7542129504611f3902c3e484d7
firedot1 <zcheng1@montgomerycollege.edu>
2020-04-14 12:12:44 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"Variables:- Declaring and Assigning Values" 
---
+++ src/VariablesExercises.java
@@ -0,0 +1,10 @@
+public class VariablesExercises {
+    public static void main(String[] args) {
+        int TestScore = 99;
+        double PriceTag = 59.99;
+
+        System.out.println("Test Score is: " + TestScore);
+        System.out.println("Price Tag: " + PriceTag);
+        System.out.println("Username: Zhi Cheng");
+    }
+}
