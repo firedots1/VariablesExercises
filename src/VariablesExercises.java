36479669208d765de4a3bf18a57202796bb01895
Zhi Cheng <63010166+firedots1@users.noreply.github.com>
2020-04-18 12:45:07 -0400

    Update VariablesExercises.java

--- src/VariablesExercises.java
+++ src/VariablesExercises.java
@@ -1,10 +1,10 @@
 public class VariablesExercises {
-    public static void main(String[] args) {
-        int TestScore = 99;
-        double PriceTag = 59.99;
+    public static void main(String[] args) {  // This particular project has two variables of integer of "TestScore" for 99. //
+        int TestScore = 99; // integer of 99 is declared. // 
+        double PriceTag = 59.99; //double of 59.99 is declared. // 
 
-        System.out.println("Test Score is: " + TestScore);
-        System.out.println("Price Tag: " + PriceTag);
-        System.out.println("Username: Zhi Cheng");
+        System.out.println("Test Score is: " + TestScore); // Prints out the following:  Test Score is: 99 // 
+        System.out.println("Price Tag: " + PriceTag); // Prints out the following:  Price Tag of 59.99. // 
+        System.out.println("Username: Zhi Cheng"); // Prints out the the following: Username: is Zhi Cheng // 
     }
 }
